657c9df6f9e66934e30de4f052553d1b7ce19ad5
DariuszSikorra <dariusz.sikorra@gmail.com>
2024-05-13 18:16:21 +0200

    Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mycie-cisnieniowe-dawexpol.pl"
+  base: "https://mycie-kostki-dawexpol.pl/"
 })
