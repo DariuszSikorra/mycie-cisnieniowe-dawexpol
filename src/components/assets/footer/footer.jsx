fba66c3ae94e6285c274ed7ca0721afc8c5abe39
DariuszSikorra <dariusz.sikorra@gmail.com>
2025-04-08 14:36:24 +0200

    anotehr logo

--- src/components/assets/footer/footer.jsx
+++ src/components/assets/footer/footer.jsx
@@ -1,28 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
 import "./footer.scss";
+
+// Example: import your logo here
+import logo from "../logo.png";
 
 const Footer = (props) => {
   return (
-    <>
+    <footer className="footer">
+      {/* Navigation Links */}
       <ul className="footer__navlinks">
-        {props.pages.map((route) => {
-          return (
-            <>
-              <li className="footer__button">
-                <NavLink
-                  className="button__link"
-                  key={route.path}
-                  to={route.path}
-                >
-                  {route.name}
-                </NavLink>
-              </li>
-            </>
-          );
-        })}
+        {props.pages.map((route) => (
+          <li className="footer__button" key={route.path}>
+            <NavLink className="button__link" to={route.path}>
+              {route.name}
+            </NavLink>
+          </li>
+        ))}
       </ul>
+
+      {/* Contact Info */}
       <ul className="footer__contact">
         <li>
           <strong>Kontakt:</strong>
@@ -30,7 +27,12 @@ const Footer = (props) => {
         <li>Krzysztof Sikorra</li>
         <li>+48 729 534 719</li>
       </ul>
-    </>
+
+      {/* Logo Section */}
+      <div className="footer__logo">
+        <img src={logo} alt="Company Logo" />
+      </div>
+    </footer>
   );
 };
 
